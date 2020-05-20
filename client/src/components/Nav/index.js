57ca553af5c1db55c9bf568cc88f186ff6e6fdcb
Chris Soltren <csoltren1@yahoo.com>
2020-05-19 22:25:16 -0400

    Fixed navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        Google Books
-      </a>
-      <a className="navbar-brand" href="/Saved"> Saved </a>
-      <a className="navbar-brand" href="/Search"> Search </a>
-    </nav>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Google Books
+    </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/search"
+          className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+        >
+          Search
+    </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/saved"
+          className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+        >
+          Saved
+    </Link>
+      </li>
+    </ul>
   );
 }
 
